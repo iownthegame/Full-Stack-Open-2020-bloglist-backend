0630465a987682f61728cb8acb443129fac694b3
Huiyu <iownthegame@gmail.com>
2020-09-19 18:38:56 +0200

    ex 4.5 - test for favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const reducer = (max, item) => (item.likes > max ? item.likes : max)
+  const maxLike = blogs.reduce(reducer, blogs[0].likes)
+  return blogs.find(blog => blog.likes === maxLike)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
