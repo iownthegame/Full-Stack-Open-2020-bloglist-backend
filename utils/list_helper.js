7c127b95063f72e4ccb1765b25ad85e28eaa8a65
Huiyu <iownthegame@gmail.com>
2020-09-19 18:19:59 +0200

    ex 4.4 - test for totalLikes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (_blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item
+  return blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
