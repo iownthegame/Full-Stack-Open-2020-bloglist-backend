2bcdf8cd246330a2fcfc2ffc96adf3ebdcb67844
Huiyu <iownthegame@gmail.com>
2020-09-20 20:44:23 +0200

    ex 4.21 - add deletion blogs with valid user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -34,7 +34,21 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(401).json({ error: 'invalid blog' })
+  }
+
+  if (blog.user.toString() !== decodedToken.id) {
+    return response.status(401).json({ error: 'invalid user' })
+  }
+
+  await blog.remove()
   response.status(204).end()
 })
 
