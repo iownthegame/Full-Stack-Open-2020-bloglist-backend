a7b5aa1040c9ecf9c6b089cebfa2ee71aa2f69cc
Huiyu <iownthegame@gmail.com>
2020-09-20 20:32:33 +0200

    ex 4.20 - add middleware.tokenExtractor

--- utils/middleware.js
+++ utils/middleware.js
@@ -28,8 +28,20 @@ const errorHandler = (error, _request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (request, _response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
